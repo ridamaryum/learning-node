ed64be3ce128c256633cc8fc839aa6a2620fcbbf
ridamaryum <maryumtahir67@gmail.com>
2024-09-08 02:13:09 -0700

    lec 5

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("maryum")
-let a=10;
-let b=20;
-let c=a+b;
-console.log("result:",c)
+"use strict";//treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser so it is not run
+
+console.log(3
+    +3); // codr readiability should be high
+console.log("rida");
