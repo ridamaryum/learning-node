af514fcf1fded876cc69aa08ae0ffa350f1f54e2
ridamaryum <maryumtahir67@gmail.com>
2024-09-15 10:35:40 -0700

    conversion.js

--- conversion.js
+++ conversion.js
@@ -5,7 +5,7 @@ console.log(typeof(score));
 
 let valueInNumber=Number(score);
 console.log(typeof valueInNumber);
-console.log(valueInNumber);*/
+console.log(valueInNumber);
 
 // "33" => 33
 // "33abs" => NaN
@@ -13,10 +13,17 @@ console.log(valueInNumber);*/
 
 let isLoggedIn="rida"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);*/
 
 // 1 => true; 0 => false
 // " " => false
 // "rida" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let 
 
 
