7f64d51a8eb947dbeabb91822307c61ddccf9b5c
ridamaryum <maryumtahir67@gmail.com>
2024-09-16 10:14:53 -0700

    operation.js

--- operation.js
+++ operation.js
@@ -5,3 +5,29 @@ console.log(posValue);
 let Value=4;
 let negValue=-value;
 console.log(negValue);
+
+
+/*console.log(2+4);
+console.log(2*4);
+console.log(16/2);
+console.log(4**4);
+console.log(2%3);*/
+
+// tricky things
+
+let str1="hello";
+let str2=" mary";
+let str3=str1+str2;
+console.log(str3);
+
+// problem
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+3+"4");
+
+
+
+
